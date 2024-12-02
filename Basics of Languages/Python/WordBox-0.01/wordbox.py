4bda8bd4ae2584ec1716efe5b08bb18c2a5d5105
Samil BOYALIKLI <sboyalikli@gmail.com>
2024-12-02 17:15:14 +0300

    There was no while loop in adding new word part. I added it.

--- Basics of Languages/Python/WordBox-0.01/wordbox.py
+++ Basics of Languages/Python/WordBox-0.01/wordbox.py
@@ -47,27 +47,48 @@ for your wordbox press 1
                 random_word["card_type"] = "Weekly"
             else: print(f"False. Answer was: {opposite}")
 else: 
-    word_dict = {
-        "card_id": "",
-        "card_name": "",
-        "card_opposite": "",
-        "card_type": "Daily",
-        "card_level": "",
-        "card_time": ""
-    }
-    new_word = input("word: ")
-    new_opposite = input("meaning: ")
-    new_card_level = input("level of the word: ")
-    word_dict["card_name"] = new_word
-    word_dict["card_opposite"] = new_opposite
-    word_dict["card_level"] = new_card_level
-    formatted_date = datetime.now().strftime("%d-%m-%Y")
-    word_dict["card_time"] = formatted_date
-    new_word_list = list(dataset.values())[1]
-    last_new_word = new_word_list[-1]
-    word_dict["card_id"] = int(last_new_word["card_id"]) + 1
-    print(word_dict)
-    dataset["new_words"].append(word_dict)
+    while True:
+        word_dict = {
+            "card_id": "",
+            "card_name": "",
+            "card_opposite": "",
+            "card_type": "Daily",
+            "card_level": "",
+            "card_time": ""
+        }
+        new_word = input("word: ")
+        new_opposite = input("meaning: ")
+        new_card_level = input("level of the word: ")
+        word_dict["card_name"] = new_word
+        word_dict["card_opposite"] = new_opposite
+        word_dict["card_level"] = new_card_level
+        formatted_date = datetime.now().strftime("%d-%m-%Y")
+        word_dict["card_time"] = formatted_date
+        new_word_list = list(dataset.values())[1]
+        last_new_word = new_word_list[-1]
+        word_dict["card_id"] = int(last_new_word["card_id"]) + 1
+        print(word_dict)
+        dataset["new_words"].append(word_dict)
+        print(
+        """
+For exit press 0
+For adding other word press 1
+        """
+            )
+        if input() == '1':
+            print(
+            """
+
+
+
+WordBox 0.01
+
+
+
+
+            """
+            )
+        else: break
 
 with open(file_path, "w") as file:
     json.dump(dataset, file, indent=4)
