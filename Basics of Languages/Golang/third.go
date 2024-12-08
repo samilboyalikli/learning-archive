387b5c72b54fdcea1845ec77478c968bb363d635
Samil BOYALIKLI <sboyalikli@gmail.com>
2024-12-08 20:48:52 +0300

    I learned inserting datas to array.

--- Basics of Languages/Golang/third.go
+++ Basics of Languages/Golang/third.go
@@ -8,6 +8,38 @@ func main() {
 	
 }
 
+// 3 - INSERTING DATAS ON THE ARRAY DIRECTLY
+// func main() {
+// 		aday := [5]int {
+// 			81,
+// 			100,
+// 			27,
+// 			95,
+// 			45,
+// 		}
+// 		var adaysayisi int = len(aday)
+// 		for i := 0; i < adaysayisi; i++ {
+// 			if aday[i] < 50 {
+// 				fmt.Println(i, ". aday başarısız oldu:",  aday[i])
+// 			}
+// 		}
+// }
+
+// 2 - EXAMPLE OF FOR LOOP WITH ARRAYS
+// func main() {
+// 		var aday [5]float32
+// 		var toplam float32
+// 		aday[0] = 81
+// 		aday[1] = 100
+// 		aday[2] = 65
+// 		aday[3] = 95
+// 		aday[4] = 45
+// 		for i := 0; i <= 4; i++ {
+// 			toplam += aday[i]
+// 		}
+// 		fmt.Println("Toplam Aday Sayısı:", len(aday))
+// 		fmt.Println("Ortalama:", toplam/5)
+// }
 
 // 1 - ARRAY
 // func main() {
