c20447e04effde9581b57cbb4b2a2385e03841bf
Samil BOYALIKLI <sboyalikli@gmail.com>
2024-12-08 16:45:26 +0300

    I added a file for second lesson for golang. And I learned arrays.

---
+++ Basics of Languages/Golang/third.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+}
+
+
+// 1 - ARRAY
+// func main() {
+// 		var il [82]string
+// 		il[1] = "Adana"
+// 		il[54] = "Sakarya"
+// 		il[16] = "Bursa"
+// 		il[47] = "Mardin"
+// 		fmt.Println(il[])
+// 		fmt.Println(il[54])
+// }
