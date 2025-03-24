12357c32db69ccb7feb4364216bbf5ce20f9a7a3
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-03-24 23:17:34 +0300

    I rearranged the file.

--- Basics of Languages/Golang/first.go
+++ Basics of Languages/Golang/first.go
@@ -4,29 +4,38 @@ import (
 	"fmt"
 )
 
-// 9.
+// 1.
 func main() {
-	isim := "Ayşe"
-	isim = "Sare"
-	memleket := "Sakarya"
-	fmt.Println(isim + memleket)
+	fmt.Println("Hello, World!")
 }
 
-// 8. 
-// REQ = reflect
+// 2.
 func main() {
-	var a = 3
-	var b = 5
-	var c = "go"
-	fmt.Println("Toplam: ", a+b)
-	fmt.Println(reflect.TypeOf(a), reflect.TypeOf(c))
+	var sayi1, sayi2 int
+	sayi1 = 3
+	sayi2 = sayi1 + 5
+	fmt.Println("Sayıların Toplamı: ", sayi1+sayi2)
 }
 
-// 7.
+// 3.
+func main()  {
+	var bolum float32
+	bolum = 8.0 / 25.0
+	fmt.Println("Bölüm: ", bolum)
+}
+// -> kullanacağımız float veri türünün daha hassas olmasını istiyorsak float32 yerine float64 kullanabiliriz.
+
+// 4.
 func main() {
-	const sakarya int = 54
-	const ülke string = "TR"
-	fmt.Println(sakarya, ülke)
+	fmt.Println("Sakarya\tBilecik\tBursa\nİzmir\tVan\tBayburt\nTürkiye")
+}
+
+// 5.
+func main() {
+	var ad, soyad string
+	ad = "Sait\tFaik"
+	soyad = "\nAbasıyanık"
+	fmt.Println(ad+soyad)
 }
 
 // 6.
@@ -41,36 +50,27 @@ func main() {
 	fmt.Println("Sakarya" == "Adapazarı")
 }
 
-// 5.
+// 7.
 func main() {
-	var ad, soyad string
-	ad = "Sait\tFaik"
-	soyad = "\nAbasıyanık"
-	fmt.Println(ad+soyad)
+	const sakarya int = 54
+	const ülke string = "TR"
+	fmt.Println(sakarya, ülke)
 }
 
-// 4.
+// 8. 
+// REQ = reflect
 func main() {
-	fmt.Println("Sakarya\tBilecik\tBursa\nİzmir\tVan\tBayburt\nTürkiye")
+	var a = 3
+	var b = 5
+	var c = "go"
+	fmt.Println("Toplam: ", a+b)
+	fmt.Println(reflect.TypeOf(a), reflect.TypeOf(c))
 }
 
-// 3.
-func main()  {
-	var bolum float32
-	bolum = 8.0 / 25.0
-	fmt.Println("Bölüm: ", bolum)
-}
-// -> kullanacağımız float veri türünün daha hassas olmasını istiyorsak float32 yerine float64 kullanabiliriz.
-
-// 2.
+// 9.
 func main() {
-	var sayi1, sayi2 int
-	sayi1 = 3
-	sayi2 = sayi1 + 5
-	fmt.Println("Sayıların Toplamı: ", sayi1+sayi2)
-}
-
-// 1.
-func main() {
-	fmt.Println("Hello, World!")
+	isim := "Ayşe"
+	isim = "Sare"
+	memleket := "Sakarya"
+	fmt.Println(isim + memleket)
 }
