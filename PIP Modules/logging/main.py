4d299804a19472e7ed2f2fa75e5b290b63cbdc92
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-04-08 01:12:52 +0300

    I modulised the codes in main file.

--- PIP Modules/logging/main.py
+++ PIP Modules/logging/main.py
@@ -1,23 +1,29 @@
 import logging
 
-log_format = "%(asctime)s - %(levelname)s - %(message)s"
-log_file = "app.log"
 
-logging.basicConfig(
-    level = logging.DEBUG,
-    format = log_format,
-    handlers = [
-        logging.FileHandler(log_file),
-        logging.StreamHandler()
-    ]
-)
+def main():
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    log_file = "app.log"
 
-sayilar = [10,5,0,20,-5]
+    logging.basicConfig(
+        level = logging.DEBUG,
+        format = log_format,
+        handlers = [
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
 
-for sayi in sayilar:
-    try:
-        sonuc = 100 / sayi
-        logging.info(f"100 / {sayi} = {sonuc}")
-    except ZeroDivisionError:
-        logging.error(f"Hata! 100 sayısı {sayi}'ya bölünemez!")
+    sayilar = [10,5,0,20,-5]
+
+    for sayi in sayilar:
+        try:
+            sonuc = 100 / sayi
+            logging.info(f"100 / {sayi} = {sonuc}")
+        except ZeroDivisionError:
+            logging.error(f"Hata! 100 sayısı {sayi}'ya bölünemez!")
+
+
+if __name__ == "__main__":
+    main()
 
