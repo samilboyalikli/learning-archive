40144cd615a554dc37973a350fdcb021be6c6701
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-04-09 22:49:08 +0300

    Added a lab works for get more understand golang dynamics.

---
+++ Basics of Languages/Golang/golab.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+)
+
+func helloWorld() {
+    fmt.Println("hello world")
+}
+
+func basicCalculator() {
+    var target int
+    fmt.Print()    
+}
+
+func main() {
+    // --> Level 1
+    // helloWorld()
+    
+}
