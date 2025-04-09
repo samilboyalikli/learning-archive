057bc61f336de20578f192cbfe90fc66d5dd24a8
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-04-09 23:42:32 +0300

    I added basicCalculator() function.

--- Basics of Languages/Golang/golab.go
+++ Basics of Languages/Golang/golab.go
@@ -8,13 +8,18 @@ func helloWorld() {
     fmt.Println("hello world")
 }
 
-func basicCalculator() {
-    var target int
-    fmt.Print()    
+func basicCalculator() string {
+    var firstTarget int
+    fmt.Print("First Target: ")
+    fmt.Scanln(&firstTarget)
+    var secondTarget int
+    fmt.Print("Second Target: ")
+    fmt.Scanln(&secondTarget)
+    return fmt.Sprintf("%d + %d", firstTarget, secondTarget)
 }
 
 func main() {
     // --> Level 1
     // helloWorld()
-    
+    fmt.Println(basicCalculator())
 }
