51d7ba5ac79522de09adedeb8f87f744a06cd57d
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-04-23 17:09:16 +0300

    Fixed a syntax problem.

--- Basics of Languages/Golang/gateway-sample/be-8082.go
+++ Basics of Languages/Golang/gateway-sample/be-8082.go
@@ -8,8 +8,7 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Second Service: %s %s", r.Method, r.URL.Path)
-        }
-    )
+        })
 
     fmt.Println("Second Backend is Working: http://localhost:8082")
     http.ListenAndServe(":8082", nil)
