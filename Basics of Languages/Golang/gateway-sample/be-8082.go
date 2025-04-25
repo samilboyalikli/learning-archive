6310de8eb99c905a57bac59f30f40d6e2eddecc4
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-04-25 21:51:54 +0300

    Fixed  some console logs.
    When program start to work, was writing "First service available" to the
    console. But be-8082 is second service, not first.
    
    I fixed this little typo.

--- Basics of Languages/Golang/gateway-sample/be-8082.go
+++ Basics of Languages/Golang/gateway-sample/be-8082.go
@@ -7,7 +7,10 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Second Service: %s %s", r.Method, r.URL.Path)
+        fmt.Fprintf(w, "Second Service Avaible...\n")
+        fmt.Fprintf(w, "Method: '%s'\n", r.Method)
+        fmt.Fprintf(w, "Host: '%s'\n", r.Host)
+        fmt.Fprintf(w, "Path: '%s'\n", r.URL.Path)
         })
 
     fmt.Println("Second Backend is Working: http://localhost:8082")
