a08d701b6336625a1afe0c04fb2e17b26e319244
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-04-26 18:37:43 +0300

    Added 4 todo to the main func.
    I added four todo article for writing rate limiter to the gateway.
    Firstly, program will save user IP with timestamp. Afterward, if same IP
    sent two request with same timestamp... Rate limiter will write an info
    log. If three, limiter will write warn log. And structure which include
    user IP's will reset all 15 minutes.

--- Basics of Languages/Golang/gateway-sample/reverseProxyByHost.go
+++ Basics of Languages/Golang/gateway-sample/reverseProxyByHost.go
@@ -39,6 +39,12 @@ func main() {
 			if strings.HasPrefix(r.Host, hostOfClient) {
 				var address string
 				address = hostOfServer+r.URL.Path
+				clientIP := r.RemoteAddr
+				// TODO - Should be saved IP with timestamp.
+				// TODO - If same IP send message with same timestamp, rate limiter will write an info log.
+				// TODO - If same IP send mesage with same timestamp, rate limiter will write a warn log.
+				// TODO - The structure which IP's saved there, will reset all 15 min.
+				log.Printf("[INFO] IP: '%s'", clientIP)
 				log.Printf("[INFO] Redirecting request...")
 				log.Printf("[INFO] Method: '%s'", r.Method)
 				log.Printf("[INFO] Adress: '%s'", address)
