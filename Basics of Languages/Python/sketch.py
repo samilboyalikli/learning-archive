bfad26f8c829fbeb23ba192f8198cacdd7ba316e
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-04-30 17:03:35 +0300

    Coded a file for decimal/hexal/octal/binaries.
    When i'm working on some problems in the platform. I tried to solve a
    Python problem about converting number type of an input. This problem
    was flagging as an easy but, I must admit that I find it so difficult.
    
    This file takes an input and returns hexadecimal, octal and binary
    equivalents of this decimal inputs and continues this process until it reaches zero.

---
+++ Basics of Languages/Python/sketch.py
@@ -0,0 +1,89 @@
+octal_mods = []
+hexadecimal_mods = []
+binary_mods = []
+
+# OCTAL PROCESSES
+
+def octal_divide(x):
+    quotient = x/8
+    mod = x%8
+    return quotient, mod
+
+
+def octal_non_zero_quotient(quotient):
+    result = octal_divide(quotient)
+    octal_mods.append(result[1])
+    return int(result[0])
+
+
+def octal_main(number):
+    check_quotient = octal_non_zero_quotient(number)
+    while check_quotient > 0:
+        number = check_quotient
+        check_quotient = octal_non_zero_quotient(number)
+
+# HEXADECIMAL PROCESSES
+
+def hexadecimal_divide(x):
+    quotient = x/16
+    mod = x%16
+    return quotient, mod
+
+
+def hexadecimal_non_zero_quotient(quotient):
+    result = hexadecimal_divide(quotient)
+    hexadecimal_mods.append(result[1])
+    return int(result[0])
+
+
+def hexadecimal_main(number):
+    check_quotient = hexadecimal_non_zero_quotient(number)
+    while check_quotient > 0:
+        number = check_quotient
+        check_quotient = hexadecimal_non_zero_quotient(number)
+
+
+# BINARY PROCESSES
+
+def binary_divide(x):
+    quotient = x/2
+    mod = x%2
+    return quotient, mod
+
+
+def binary_non_zero_quotient(quotient):
+    result = binary_divide(quotient)
+    binary_mods.append(result[1])
+    return int(result[0])
+
+
+def binary_main(number):
+    check_quotient = binary_non_zero_quotient(number)
+    while check_quotient > 0:
+        number = check_quotient
+        check_quotient = binary_non_zero_quotient(number)
+
+# CONVERTING
+
+def binary_calculator(n):
+    for i in range(1,n+1):
+        global binary_mods
+        global octal_mods
+        global hexadecimal_mods
+        # todo - 10/A | 11/B | 12/C | 13/D | 14/E | 15/F
+        binary_main(i)  
+        octal_main(i)  
+        hexadecimal_main(i)
+        binary_result = ''.join(map(str,binary_mods[::-1]))
+        octal_result = ''.join(map(str,octal_mods[::-1])) 
+        hexadecimal_result = ''.join(map(str,hexadecimal_mods[::-1]))
+        print(i,octal_result,hexadecimal_result,binary_result)
+        binary_mods = []
+        octal_mods = []
+        hexadecimal_mods = []
+
+if __name__ == '__main__':
+    #n = int(input())
+    #print_formatted(n)
+    binary_calculator(10)   
+
