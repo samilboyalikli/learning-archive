74de0dad40dd1aeff36a1dcee0233545a703fc1d
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-05-11 12:51:26 +0300

    Loop block added to the file.
    Firstly, I added a code block for divison after arithmetic operations.
    Afterward, added a loop which include range keyword. There is an
    important point: I used range with 0, but as I understand default
    starting is 0 into the range parameters.
    
    So after understanding this fact, I deleted 0 directly from range().

--- Basics of Languages/Python/some_solutions.py
+++ Basics of Languages/Python/some_solutions.py
@@ -1,4 +1,43 @@
 
+def others_loops():
+    n = int(input())
+    results = [i*i for i in range(n)]
+    print(*results,sep='\n')
+
+
+def sb_loops():
+    n = int(input())
+    for i in range(n):
+        print(i*i)
+
+
+def sb_python_division():
+    a = int(input())
+    b = int(input())
+    print(f"{a//b}\n{a/b}")
+
+
+def others_arithmetic_operations():
+    a = int(input())
+    b = int(input())
+    results = [a+b,a-b,a*b]
+    print(*results,sep='\n')
+
+
+def sb_arithmetic_operations():
+    a = int(input())
+    b = int(input())
+    print(f"{a+b}\n{a-b}\n{a*b}")
+
+
+def others_python_if_else():
+    n = int(input().strip())
+    if n%2==1 or 6<=n<=20:
+        print("Weird")
+    elif 2<=n<=5 or n>=20:
+        print("Not Weird")
+
+
 def sb_python_if_else():
     n = int(input().strip())
     if n%2!=0:
@@ -12,30 +51,14 @@ def sb_python_if_else():
             print("Not Weird")   
 
 
-def others_python_if_else():
-    n = int(input().strip())
-    if n%2==1 or 6<=n<=20:
-        print("Weird")
-    elif 2<=n<=5 or n>=20:
-        print("Not Weird")
-
-
-def sb_arithmetic_operations():
-    a = int(input())
-    b = int(input())
-    print(f"{a+b}\n{a-b}\n{a*b}")
-
-
-def others_arithmetic_operations():
-    a = int(input())
-    b = int(input())
-    results = [a+b,a-b,a*b]
-    print(*results,sep='\n')
-
-
 if __name__ == '__main__':
     #sb_python_if_else() 
     #others_python_if_else()  
     #sb_arithmetic_operations()
-    others_arithmetic_operations()
+    #others_arithmetic_operations()
+    #sb_python_division()
+    #sb_loops()
+    others_loops()
+
+
 
