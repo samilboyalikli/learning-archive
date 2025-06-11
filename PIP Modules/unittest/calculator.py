c87a8291f41668588f8a8f078767d42c68d03fec
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-06-11 22:22:05 +0300

    Simple calculator file added for testing.
    I want to learn how I write unittest. For learning that, I will try to
    write a test file for another python file.
    
    I added "another file" in this commit. There is four functions:
    addition(), subtraction(), multiplication(), divison(). Understandable
    names meaning what you understand.

---
+++ PIP Modules/unittest/calculator.py
@@ -0,0 +1,17 @@
+
+def addition(x,y):
+    return int(x)+int(y)
+
+
+def subtraction(x,y):
+    return int(x)-int(y)
+
+
+def multiplication(x,y):
+    return int(x)*int(y)
+
+
+def division(x,y):
+    return int(x)/int(y)
+
+
