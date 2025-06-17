2af4d1d285481697736765a75a59fa14f84eeabf
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-06-17 23:53:04 +0300

    Connected html page with main.go
    Second todo was creating a connection between home.html and main.go, and
    with this commit I solved that.

--- Basics of Languages/Golang/be-samples/firstLevel/main.go
+++ Basics of Languages/Golang/be-samples/firstLevel/main.go
@@ -6,6 +6,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Backend is Working: http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Service available...\n")
+    })
+	fmt.Println("Backend is Working: http://localhost:8080/home.html")
+    page := http.FileServer(http.Dir("templates"))
+    http.Handle("/", page)
+    err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+        panic(err)
+    }
 }
+
+// TODOS
+// 1. wordsaving component in home.html (checked)
+// 2. main.go will be connected with home.html 
+// 3. creating sqlite database
+// 4. database structure
