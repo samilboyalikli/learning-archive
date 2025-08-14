bda437c8948f9a997b89df617d108099f6a7f273
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-08-14 20:52:34 +0300

    Added to routes other one.
    The file had single route. I increased the routes with this commit.

--- PIP Modules/flask/listener.py
+++ PIP Modules/flask/listener.py
@@ -8,3 +8,11 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/hello")
+def hello():
+    return "<p>Hello!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
