6242f8c1359b0d7d219c1b9acfb71463d80da233
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-08-17 23:21:17 +0300

    Added http module version of flask code blocks.
    There was a code block which can take http get request. I coded a new
    code block also can take http get request but I didn't use flask module.

--- PIP Modules/flask/listener.py
+++ PIP Modules/flask/listener.py
@@ -1,3 +1,5 @@
+# ---------------- Flask Application ----------------
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -20,6 +22,24 @@ def triggerpoint():
         return f"<p>File is triggered.</p>", 200
     else:
         return "<p>File did not trigger.</p>", 400
+
+
+# ---------------- HTTP Application ----------------
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<p>Hello, World!</p>")
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"<p>Not Found</p>")
 
 
 if __name__ == "__main__":
