e62d8bd2ae18e2be205fda0dd70fa58c862f1935
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-08-17 23:23:28 +0300

    Used functions in the main function.
    I coded a class which include two functions. These two func can take
    http get request with http static module.
    
    I used these functions in the main func with this commit.

--- PIP Modules/flask/listener.py
+++ PIP Modules/flask/listener.py
@@ -43,5 +43,9 @@ class MyHandler(BaseHTTPRequestHandler):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, MyHandler)
+    print("Starting HTTP server on port 8000...")
+    httpd.serve_forever()
