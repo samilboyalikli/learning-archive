887df9c466041d74441715ace426bac160eccf9d
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-10-13 16:50:37 +0300

    [feat] bs4: Added 'availability' component to the main scraper.

--- PIP Modules/bs4/main.py
+++ PIP Modules/bs4/main.py
@@ -5,30 +5,43 @@ import csv
 
 
 @lru_cache(maxsize=32)
-def fetch_page(url):
-    response = requests.get(url)
-    return response
+def pagination(path):
+    response = requests.get(f"https://books.toscrape.com/{path}")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
 
 
-soup = BeautifulSoup(fetch_page("https://books.toscrape.com/catalogue/category/books_1/index.html").content, 'html.parser')
-soup.find_all('a', limit=5)
-
-
-def book_and_price():
-    book_components = soup.find_all('article')
+def book_price_availability(path):
+    book_components = pagination(path).find_all('article')
     dataset = []    
     
     for book in book_components:
         line = []
         line.append(book.find('h3').find('a').get('title'))
         line.append(book.find_all('div')[1].find_all('p')[0].text)
+        line.append(book.find_all('div')[1].find_all('p')[1].get_text(strip=True))
         dataset.append(line)
     
     return dataset
+    
+    button = pagination(path).find(class_='next')
+    if button:
+        return button.find('a').get('href')
+    else:
+        return 0
 
 
 def main():
-    print(book_and_price())
+    if book_price_availability("index.html"):
+        print(book_price_availability("index.html"))
+    else: 
+        print("0")
+    #button = pagination("index.html").find(class_='next')
+    
+    #if button:
+        #next_page_url = button.find('a').get('href')
+        #book_price_availability(next_page_url)
+        
 
 
 if __name__ == "__main__":
