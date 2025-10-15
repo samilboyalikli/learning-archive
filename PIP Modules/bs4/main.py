68230783d01241c8c16cd8214e8a01f6a629aa92
Samil BOYALIKLI <sboyalikli@gmail.com>
2025-10-15 16:19:18 +0300

    [feat] bs4: Finished pagination strategy.
    After scraping one page, it was impossible to move on to the next.
    
    This problem was solved with the help of two global variables.

--- PIP Modules/bs4/main.py
+++ PIP Modules/bs4/main.py
@@ -3,45 +3,61 @@ from bs4 import BeautifulSoup
 from functools import lru_cache 
 import csv
 
+path = "index.html"
+result = []
+
 
 @lru_cache(maxsize=32)
-def pagination(path):
-    response = requests.get(f"https://books.toscrape.com/{path}")
+def pagination(url_path):
+    response = requests.get(f"https://books.toscrape.com/{url_path}")
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup
 
 
-def book_price_availability(path):
-    book_components = pagination(path).find_all('article')
-    dataset = []    
+def book_price_availability(url_path):
+    global path
+    global result
+    book_components = pagination(url_path).find_all('article')
     
     for book in book_components:
         line = []
         line.append(book.find('h3').find('a').get('title'))
         line.append(book.find_all('div')[1].find_all('p')[0].text)
         line.append(book.find_all('div')[1].find_all('p')[1].get_text(strip=True))
-        dataset.append(line)
+        result.append(line)
     
-    return dataset
-    
-    button = pagination(path).find(class_='next')
+    button = pagination(url_path).find(class_='next')
+
     if button:
-        return button.find('a').get('href')
+        button_href = button.find('a').get('href')
+        if button_href[:10] == "catalogue/":
+            path = button_href
+        else:
+            path = f"catalogue/{button_href}"
     else:
+        print(f"INFO - There is no next button in this page {url_path}")
         return 0
 
 
+def saving_results(csv_file, dataset, headers):
+    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+        open_csv_file = csv.writer(file)
+        open_csv_file.writerow(headers)
+        for row in dataset:
+            open_csv_file.writerow(row)
+
+
 def main():
-    if book_price_availability("index.html"):
-        print(book_price_availability("index.html"))
-    else: 
-        print("0")
-    #button = pagination("index.html").find(class_='next')
+    while True:
+        status = book_price_availability(path)
+        if status == 0:
+            break 
+        print(f"INFO - {path} scraped.")
     
-    #if button:
-        #next_page_url = button.find('a').get('href')
-        #book_price_availability(next_page_url)
-        
+    try: 
+        saving_results('result.csv', result, ["Book Name", "Price", "Availability"])
+    except Exception as e: 
+        print("ERROR - ", e)
 
 
 if __name__ == "__main__":
