595706d846e6e4913572b7fbec43436031821566
Samil BOYALIKLI <sboyalikli@gmail.com>
2026-03-08 08:37:38 +0300

    [ai-dev] feat: benchmark structure completed.

--- ai-dev/embedding/custom-sts-benchmark/main.py
+++ ai-dev/embedding/custom-sts-benchmark/main.py
@@ -3,12 +3,7 @@
 # from numpy.linalg import norm
 import json
 
-
-# e1 = model.encode(first_text)
-# e2 = model.encode(second_text)
-
-# similarity = dot(e1, e2)/(norm(e1)*norm(e2))
-# print(similarity)
+result = {}
 
 
 def examples():
@@ -21,20 +16,27 @@ def models():
         return json.load(file)
 
 
-def tester(level, model):
+def cosine_similarity(model, first_word, second_word):
     # embedding_model = SentenceTransformer(model)
+    # e1 = model.encode(first_text)
+    # e2 = model.encode(second_text)
+    # similarity = dot(e1, e2)/(norm(e1)*norm(e2))
+    # print(similarity)
+    return 0.75
+
+
+def tester(level, model):
     if level == "simple":
         dataset = examples()["simple"]
         for data in dataset:
             first_word = list(data.values())[0][0]
             second_word = list(data.values())[0][1]
             result_dictionary = {
-                "Model Name": model,
-                "Cosine Similarity": 0.75,
+                "Cosine Similarity": cosine_similarity(model, first_word, second_word),
                 "First Word": first_word,
                 "Second Word": second_word
             }
-            print(result_dictionary)
+            result.setdefault(model, []).append(result_dictionary)
 
 
 def global_model_tester():
@@ -47,6 +49,8 @@ def global_model_tester():
 
 def main():
     global_model_tester()
+    with open("output.json", "w", encoding='utf-8') as file:
+        json.dump(result, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
